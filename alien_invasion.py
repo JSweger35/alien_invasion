8bfe71d4084e6b17d3a76ba4ca301a853f2ea62d
Jake S <jsweg13@gmail.com>
2023-06-19 21:35:12 -0400

    set the first instance of the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,3 +5,26 @@ import pygame
 Class AlienInvasion:
     """Overall Class to manage game assets and behavior"""
     
+    def_init_(self)
+    """Initialize the game, and create game resources"""
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        
+    def run_game(self)
+    """Start the main loop for the game"""
+        while True:
+            # watch for keyboard and mouse events
+            for event in pygame.event.get()
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            
+if_name_=='_main_':
+    #Make a gmae instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+    
