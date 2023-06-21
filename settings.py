80f8853f35ca98eb3cd3d1ccd5cacc32199d0cd5
Jake Sweger <jsweg13@gmail.com>
2023-06-21 18:12:50 -0400

    change windows resolution

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 2560
+        self.screen_height = 1080
         self.bg_color = (0, 0, 0)
