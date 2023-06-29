f2a79db3f0178630b4e927c970db1a76b6107590
Jake S <jsweg13@gmail.com>
2023-06-28 21:36:56 -0400

    Add ability to go left

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,13 +40,19 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     #move character to the right
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    #move character left
+                    self.ship.moving_left = True
+                    
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.type == pygame.K_LEFT:
+                    self.ship.moving_left = False
                     
             
     def _update_screen(self):
-        """Update images on the sccreen, and flip to the new screen.""" 
+        """Update images on the screen, and flip to the new screen.""" 
         # Redraw the screen during each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
