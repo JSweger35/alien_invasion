701aa065e9cad8b891411dcf2dd8085b2cf853c1
Jake S <jsweg13@gmail.com>
2023-07-25 12:19:01 -0400

    Add new __init__ method

--- game_stats.py
+++ game_stats.py
@@ -1,2 +1,7 @@
 Class GameStats:
     """Track statistics for Alien Invasion."""
+    
+    def __init__(self, ai_game):
+        """Initialize statistics"""
+        self.settings = ai_game.settings
+        self.reset_stats()
