818f25ce48bd99a1bb6737df51ea1e5e5ebcb444
Jake S <jsweg13@gmail.com>
2023-08-09 12:22:44 -0400

    reset stats on mouse press

--- alien_invasion.py
+++ alien_invasion.py
@@ -170,6 +170,8 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play"""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game stats
+            self.stats.reset_stats()
             self.stats.game_active = True
                 
     def _check_keydown_events(self,event):
