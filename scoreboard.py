ef9e4b5b9a9532e9f84ac90bdd5a34e87cc1bf03
Jake S <jsweg13@gmail.com>
2023-08-17 12:20:15 -0400

    fix typo of score

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,7 @@ class Scoreboard:
     def prep_score(self):
         """Turn the score into a rendered image."""
         score_str = str(self.stats.score)
-        self.score_image = self.font.render(scor_str, True, self.text_color, self.settings.bg_color)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
         
         # Display the score at the top right of the screen. 
         self.score_rect = self.score_image.get_rect()
