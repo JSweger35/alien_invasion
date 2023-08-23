bfebd91ae565817c06502a6dfcd06a08f993e93e
Jake S <jsweg13@gmail.com>
2023-08-23 12:28:44 -0400

    Add high score method

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,17 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # Center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+                
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
