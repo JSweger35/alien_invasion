d407f342fd4ad8235296388ad9b75ba88654d8c8
Jake S <jsweg13@gmail.com>
2023-08-28 12:30:57 -0400

    Add prep_level

--- scoreboard.py
+++ scoreboard.py
@@ -18,6 +18,16 @@ class Scoreboard:
         self.prep_high_score()
         self.prep_level()
         
+    def prep_level(self):
+        """Turn the level into a rendered image"""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+        
+        # Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+    
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
