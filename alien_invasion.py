7509aa1389a51301cdece35d682aff37e6de7599
Jake S <jsweg13@gmail.com>
2023-08-30 12:31:38 -0400

    Call prep_ships instead of ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -150,8 +150,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to ship being hit by alien"""
         if self.stats.ships_left > 0:
-            # Decrement ships_left
+            # Decrement ships_left, and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
