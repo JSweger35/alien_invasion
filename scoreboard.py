b87f4abf60cfe0335829ce1b6f1be0c7a827a39d
Jake S <jsweg13@gmail.com>
2023-08-30 12:21:15 -0400

    add self

--- scoreboard.py
+++ scoreboard.py
@@ -34,7 +34,7 @@ class Scoreboard:
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
         
-    def prep_ships():
+    def prep_ships(self):
         """Show how many ships are left."""
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
