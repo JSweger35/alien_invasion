0bd00285495d535d70a36e642ccfcb2a4404d966
Jake S <jsweg13@gmail.com>
2023-08-30 12:24:21 -0400

    include ships in check play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -192,6 +192,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             # hide mouse cursor after game starts
             pygame.mouse.set_visible(False)
             
